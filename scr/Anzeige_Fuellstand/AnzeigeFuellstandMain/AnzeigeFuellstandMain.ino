39ee33cba2da7084e26a7034393f7dc0d18265a8
Xernito <newwave1983@web.de>
2021-08-20 10:38:11 +0200

    Update AnzeigeFuellstandMain.ino

--- scr/Anzeige_Fuellstand/AnzeigeFuellstandMain/AnzeigeFuellstandMain.ino
+++ scr/Anzeige_Fuellstand/AnzeigeFuellstandMain/AnzeigeFuellstandMain.ino
@@ -1,46 +1,128 @@
-#include <Adafruit_MCP23X17.h>
-
-Adafruit_MCP23X17 mcp_1;
-Adafruit_MCP23X17 mcp_2;
+#include <Wire.h>
 
 void setup() {
-  // put your setup code here, to run once:
-  mcp_1.begin_I2C(0x20);
-  mcp_2.begin_I2C(0x24);
-  for(int i=0;i<=15;i++)
+  
+  Wire.begin();
+  
+  //Expander 1 - set all of PINs to outputs
+  Wire.beginTransmission(0x20); //A0 = A1 = A2 = 0
+  Wire.write(0x00); // IODIRA register
+  Wire.write(0x00); // set all of port A to outputs
+  Wire.endTransmission();
+  Wire.beginTransmission(0x20);
+  Wire.write(0x01); // IODIRB register
+  Wire.write(0x00); // set all of port B to outputs
+  Wire.endTransmission();
+  
+  //Expander 2 - set all of PINs to outputs
+  Wire.beginTransmission(0x24); //A0 = A1 = 0; A2 = 1
+  Wire.write(0x00); // IODIRA register
+  Wire.write(0x00); // set all of port A to outputs
+  Wire.endTransmission();
+  Wire.beginTransmission(0x24);
+  Wire.write(0x01); // IODIRB register
+  Wire.write(0x00); // set all of port B to outputs
+  Wire.endTransmission();
+
+  //Let all LEDs flash briefly 
+  
+  for(byte i=0;i<3;i++)
   {
-    mcp_1.pinMode(i,OUTPUT);
-    mcp_1.digitalWrite(i,LOW);
-    mcp_2.pinMode(i,OUTPUT);
-    mcp_1.digitalWrite(i,LOW);
+    setAllPins(0x20,0xFF); //Expander 1
+    setAllPins(0x24,0xFF); //Expander 2
+    delay(500);
+    setAllPins(0x20,0x00); //Expander 1
+    setAllPins(0x24,0x00); //Expander 2
+    delay(500);
   }
+  
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-    for(int i=0;i<=14;i++)
-  {
-    mcp_1.digitalWrite(i,HIGH);
-    delay(250);
-  }
-    for(int i=0;i<=14;i++)
-  {
-    mcp_2.digitalWrite(i,HIGH);
-    delay(250);
-  }
+void setAllPins(uint8_t address, uint8_t value)
+{
+  setValueRegA(address, value);
+  setValueRegB(address, value);
+}
 
-  delay(1000);
+void setValueRegA(uint8_t address, uint8_t value)
+{
+  Wire.beginTransmission(address); //A0 = A1 = A2 = 0
+  Wire.write(0x12); // address port A
+  Wire.write(value); // set all Bit to 1
+  Wire.endTransmission();
+}
 
-      for(int i=0;i<=14;i++)
+void setValueRegB(uint8_t address, uint8_t value)
+{
+  Wire.beginTransmission(address); //A0 = A1 = A2 = 0
+  Wire.write(0x13); // address port B
+  Wire.write(value); // set all Bit to 1
+  Wire.endTransmission();
+}
+
+void setValueByPercent_S1(byte percent)
+{
+  switch (percent)
   {
-    mcp_1.digitalWrite(i,LOW);
-    delay(250);
-  }
-    for(int i=0;i<=14;i++)
-  {
-    mcp_2.digitalWrite(i,LOW);
-    delay(250);
-  }
+    case 0:
+      setValueRegA(0x20,0x00);  //0000 0000
+      //setValueRegB(0x20,??);    //0000 0000
+      break;
+    
+    case 10:
+      setValueRegA(0x20,0x01);  //0000 0001
+      //setValueRegB(0x20,??);    //0000 0000
+      break;
+      
+     case 20:
+      setValueRegA(0x20,0x03);  //0000 0011
+      //setValueRegB(0x20,??);    //0000 0000
+      break;
+      
+    case 30:
+      setValueRegA(0x20,0x07);  //0000 0111
+      setValueRegB(0x20,??);    //0000 0000
+      break;
+    
+    case 40:
+      setValueRegA(0x20,0x0F);  //0000 1111
+      setValueRegB(0x20,??);    //0000 0000
+      break;
+      
+     case 50:
+      setValueRegA(0x20,0x1F);  //0001 1111
+      //setValueRegB(0x20,??);    //0000 0000
+      break;
+      
+    case 60:
+      setValueRegA(0x20,0x3F);  //0011 1111
+      //setValueRegB(0x20,??);    //0000 0000
+      break;
+    
+    case 70:
+      setValueRegA(0x20,0x7F);  //0111 1111
+      //setValueRegB(0x20,??);    //0000 0000
+      break;
+      
+     case 80:
+      setValueRegA(0x20,0xFF);  //1111 1111
+      setValueRegB(0x20,??);    //0000 0000
+      break;
+      
+     case 90:
+      setValueRegA(0x20,0xFF);  //1111 1111
+      setValueRegB(0x20,??);    //0000 0001
+      break;
+      
+     case 100:
+      setValueRegA(0x20,0xFF);  //1111 1111
+      setValueRegB(0x20,??);    //0000 0011
+      break;
+}
+
+void loop()
+{
+  
 }
 
 
@@ -54,56 +136,3 @@ void serialEvent()
 
   Serial.println(inputString);
 }
-
-/*
-  Entfernung = uS/US_ROUNDTRIP_CM;
-
-  //120 cm == leer
-  //20 cm == voll
-  //100 cm Füllstand
-
-  
-  
-  Count = Entfernung / 10;
-  
-  if(Entfernung !=0) 
-  {
-    //alle je cm 1 LED - max. 10cm
-    if(Count_alt != Count)
-    {
-    
-      for(int i=0;i<=Count;i++)
-      {
-        mcp.digitalWrite(i, LOW);
-      }
-      if(Count <= 10)
-      {
-        for(int i=Count;i<=10;i++)
-        {
-          mcp.digitalWrite(i, HIGH);
-        }
-      }
-
-      Count_alt = Count;
-    }
-    
-  }
-  else
-  {
-    if(Count_alt != Count)
-    {
-      for(int i=0;i<=10;i++)
-      {
-        if(i%2)
-        {
-          mcp.digitalWrite(i, HIGH);
-        }
-        else
-        {
-          mcp.digitalWrite(i, LOW);
-        }
-      }
-      //Count_alt = Count;
-    }
-  }
-*/
